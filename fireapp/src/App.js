ea344de8623f526818568cbcdd0c60d93df11ff3
Kaio Vinicius Morais Pereira <kaiovincius@MacBook-Air-de-Kaio.local>
2023-09-27 23:55:12 -0300

    configurações do banco de dados do meu firebase

---
+++ fireapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {db} from './FirebaseConnection';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ReactJs + Firebase</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ fireapp/src/FirebaseConnection.js
@@ -0,0 +1,20 @@
+import { initializeApp} from 'firebase/app'
+import {getFirestore} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDO8K1rUYa7GlNa4-hyICTw1XggBCEQLp0",
+    authDomain: "cursoreact-d0a18.firebaseapp.com",
+    projectId: "cursoreact-d0a18",
+    storageBucket: "cursoreact-d0a18.appspot.com",
+    messagingSenderId: "1010205240927",
+    appId: "1:1010205240927:web:60516ea035239340fdb5ea",
+    measurementId: "G-SZ42W8GR5Y"
+  };
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+const db = getFirestore(firebaseApp)
+
+export {db}
+
+//configurações para aceesar o banco do firebase aqui na nossa aplicacao
