457145f5db810356fa4fe64fd06bd35b07444d05
Kaio Vinicius Morais Pereira <kaiovincius@MacBook-Air-de-Kaio.local>
2023-10-01 13:30:37 -0300

    criação da minha pagina home

--- fireapp/src/pages/Home/index.js
+++ fireapp/src/pages/Home/index.js
@@ -1,7 +1,47 @@
+import { useState } from 'react'
+import '../Home/home.css'
+import { Link } from 'react-router-dom'
+
 export default function Home() {
-    return (
-      <>
-        <h1>Pagina Home</h1>
-      </>
-    )
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+
+  function handleLogin(e){
+    e.preventDefault();//para ele previnir o comportamento e não atualizar a pagina(por conta de ser um form)
+    
+    if(email !== '' && senha !== ''){
+      alert('seguindo para a proxima pagina')
+    }
+    else{
+      alert('preencha todos os campo para prosseguir!!')
+    }
+  }
+  return (
+    <div className='home-container'>
+      <h1>Lista de tarefas</h1>
+      <span>Gerencie sua agenda de forma facil.</span>
+
+      <form className='form' onSubmit={handleLogin}>
+        <input
+          type='text'
+          placeholder='digite seu email...'
+          value={email}
+          onChange={(e) => { setEmail(e.target.value) }}
+        />
+
+        <input
+          autoComplete={false}
+          type='text'
+          placeholder='digite sua senha...'
+          value={senha}
+          onChange={(e) => { setSenha(e.target.value) }}
+        />
+
+        <button type='submit'>Acessar</button>
+        <Link className='button-link' to='/register'>
+          Não possui uma conta? Cadastre-se
+        </Link>
+      </form>
+    </div>
+  )
 }
